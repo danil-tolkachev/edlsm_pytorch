74f45139058840ffe29955b4daa60da9746ec351
Danil Tolkachev <tolkachev@nkbvs.ttn.ru>
2023-04-24 17:56:14 +0300

    remove tf dependency

--- train.py
+++ train.py
@@ -1,45 +1,103 @@
 import os
 import pprint
-import tensorflow as tf
 from src.edlsm_learner import edlsmLearner
+import argparse
 
-flags = tf.app.flags
-flags.DEFINE_string("directory", "./data_scene_flow/training", "Directory to the dataset")
-flags.DEFINE_string("train_val_split_dir", "./dataset", "Directory to the dataset")
-flags.DEFINE_string("train_dataset_name", "tr_18_18_100.txt", "Training set")
-flags.DEFINE_string("val_dataset_name",   "val_5_18_100.txt", "Validation set")
-flags.DEFINE_string("checkpoint_dir", "./checkpoints", "Directory name to save the checkpoints")
-flags.DEFINE_string("logs_path", "logs", "Tensorboard log path")
-flags.DEFINE_bool("continue_train", True, "Resume training")
-flags.DEFINE_bool("gpu", True, "Use GPU if available")
-flags.DEFINE_string("init_checkpoint_file", "edlsm_24000.ckpt", "checkpoint file")
-flags.DEFINE_integer("batch_size", 128, "The size of of a sample batch")
-flags.DEFINE_integer("psz", 18, "Left patch size")
-flags.DEFINE_integer("half_range", 100, "Right patch half range")
-flags.DEFINE_integer("image_width", 1238, "Image width, must same as prepare_dataset")
-flags.DEFINE_integer("image_height", 375, "Image height, must same as prepare_dataset")
-flags.DEFINE_integer("start_step",24000,    "Starting training step")
-flags.DEFINE_integer("max_steps", 50000, "Maximum number of training iterations")
-flags.DEFINE_float("l_rate", 0.01, "learning rate")
-flags.DEFINE_float("l2", 0.0005, "Weight Decay")
-flags.DEFINE_integer("reduce_l_rate_itr", 8000, "Reduce learning rate after this many iterations")
-flags.DEFINE_float("pxl_wghts", [[1.0, 4.0, 10.0, 4.0, 1.0]], "Weights for three pixel error")
-flags.DEFINE_integer("summary_freq", 200, "Logging every summary_freq iterations")
-flags.DEFINE_integer("valid_freq",   500, "Logging every valid_freq iterations")
-flags.DEFINE_integer("save_latest_freq", 2000, \
-                       "Save the latest model every save_latest_freq iterations")
-FLAGS = flags.FLAGS
+parser = argparse.ArgumentParser()
 
-def main(_):
+parser.add_argument("--directory",
+                    type=str,
+                    default="./data_scene_flow/training",
+                    help="Directory to the dataset")
+parser.add_argument("--train_val_split_dir",
+                    type=str,
+                    default="./dataset",
+                    help="Directory to the dataset")
+parser.add_argument("--train_dataset_name",
+                    type=str,
+                    default="tr_160_18_100.txt",
+                    help="Training set")
+parser.add_argument("--val_dataset_name",
+                    type=str,
+                    default="val_40_18_100.txt",
+                    help="Validation set")
+parser.add_argument("--checkpoint_dir",
+                    type=str,
+                    default="./checkpoints",
+                    help="Directory name to save the checkpoints")
+parser.add_argument("--logs_path",
+                    type=str,
+                    default="logs",
+                    help="Tensorboard log path")
+parser.add_argument("--continue_train",
+                    action='store_true',
+                    help="Resume training")
+parser.add_argument("--gpu", action='store_true', help="Use GPU if available")
+parser.add_argument("--init_checkpoint_file",
+                    type=str,
+                    default="edlsm_24000.ckpt",
+                    help="checkpoint file")
+parser.add_argument("--batch_size",
+                    type=int,
+                    default=128,
+                    help="The size of of a sample batch")
+parser.add_argument("--psz", type=int, default=18, help="Left patch size")
+parser.add_argument("--half_range",
+                    type=int,
+                    default=100,
+                    help="Right patch half range")
+parser.add_argument("--image_width",
+                    type=int,
+                    default=1238,
+                    help="Image width, must same as prepare_dataset")
+parser.add_argument("--image_height",
+                    type=int,
+                    default=375,
+                    help="Image height, must same as prepare_dataset")
+parser.add_argument("--start_step",
+                    type=int,
+                    default=24000,
+                    help="Starting training step")
+parser.add_argument("--max_steps",
+                    type=int,
+                    default=50000,
+                    help="Maximum number of training iterations")
+parser.add_argument("--l_rate", type=float, default=0.01, help="learning rate")
+parser.add_argument("--l2", type=float, default=0.0005, help="Weight Decay")
+parser.add_argument("--reduce_l_rate_itr",
+                    type=int,
+                    default=8000,
+                    help="Reduce learning rate after this many iterations")
+parser.add_argument("--pxl_wghts",
+                    type=float,
+                    default=[[1.0, 4.0, 10.0, 4.0, 1.0]],
+                    help="Weights for three pixel error")
+parser.add_argument("--summary_freq",
+                    type=int,
+                    default=200,
+                    help="Logging every summary_freq iterations")
+parser.add_argument("--valid_freq",
+                    type=int,
+                    default=500,
+                    help="Logging every valid_freq iterations")
+parser.add_argument(
+    "--save_latest_freq",
+    type=int,
+    default=2000,
+    help="Save the latest model every save_latest_freq iterations")
+args = parser.parse_args()
+
+
+def main():
     pp = pprint.PrettyPrinter()
-    pp.pprint(flags.FLAGS.__flags)
+    pp.pprint(vars(args))
 
-    if not os.path.exists(FLAGS.checkpoint_dir):
-        os.makedirs(FLAGS.checkpoint_dir)
+    if not os.path.exists(args.checkpoint_dir):
+        os.makedirs(args.checkpoint_dir)
 
     edlsm = edlsmLearner()
-    edlsm.train(FLAGS)
+    edlsm.train(args)
 
 
 if __name__ == '__main__':
-    tf.app.run()
+    main()
